a69e7facd75e12e0f3a7b9bc029112e7da886491
Khurshiddd <marifovxurshid80@gmail.com>
2023-09-30 21:33:59 +0500

    add

--- src/components/desks/Index.vue
+++ src/components/desks/Index.vue
@@ -1,98 +1,152 @@
 <template>
-    <div class="container">
-        <form>
-            <div class="form-group">
-                <label for="namein" class="label-group">Name Desk</label>
-                <input type="text" id="namein" class="form-control my-3" v-model="name" required>
-                <div v-if="errorMessage" class="text-danger" >
-                    {{ errorMessage }}
-                </div>
-            </div>
-            <button type="submit" @click.prevent="addDesk" class="btn btn-primary my-3">Add Desk</button>
-        </form>
-        <div class="row">
-            <div class="col-lg-4" v-for="desk in desks">
-                <div class="card mt-3">
-                    <router-link :to="{name: 'show', params: {deskId: desk.id}}" class="card-body">
-                        <h5 class="card-title text-center">{{ desk.name }}</h5>
-                    </router-link>
-                    <button type="button" class="btn btn-danger" @click="deleteDesk(desk.id)">Delete</button>
-                </div>
-            </div>
+  <div class="container">
+    <form>
+      <div class="form-group">
+        <label for="namein" class="label-group">Name Desk</label>
+        <input
+          type="text"
+          id="namein"
+          class="form-control my-3"
+          v-model="name"
+          required
+        />
+        <div v-if="errorMessage" class="text-danger">
+          {{ errorMessage }}
         </div>
-        <div class="alert alert-danger" role="alert" v-if="errored">
-            error loading data
-        </div>
-        <div class="d-flex justify-content-center" v-if="isLoading">
-            <div class="text-center spinner-border text-primary" role="status">
-                <span class="visually-hidden">Loading...</span>     
-            </div>
-            <h2 class="text-primary">Loading...</h2>
-        </div>
+      </div>
+      <button type="submit" @click.prevent="addDesk" class="btn btn-primary my-3">
+        Add Desk
+      </button>
+    </form>
+    <div class="d-flex justify-content-center" v-if="isLoading">
+      <div class="text-center spinner-border text-primary" role="status">
+        <span class="visually-hidden">Loading...</span>
+      </div>
+      <h2 class="text-primary">Loading...</h2>
     </div>
+    <div class="row">
+      <div class="col-lg-4" v-for="desk in desks">
+        <div class="card mt-3">
+          <router-link
+            :to="{ name: 'show', params: { deskId: desk.id } }"
+            class="card-body"
+          >
+            <h5 class="card-title text-center">{{ desk.name }}</h5>
+          </router-link>
+          <button type="button" class="btn btn-danger" @click="deleteDesk(desk.id)">
+            Delete
+          </button>
+        </div>
+      </div>
+    </div>
+    <div class="mt-4 d-flex justify-content-center align-items-center" v-if="!isLoading">
+      <nav aria-label="Page navigation example">
+        <ul class="pagination">
+          <!-- <li class="page-item">
+                        <a class="page-link" href="#" aria-label="Previous">
+                            <span aria-hidden="true">&laquo;</span>
+                        </a>
+                    </li> -->
+          <li
+            class="page-item d-flex"
+            style="cursor: pointer"
+            :class="{ active: pageNumber == page }"
+            v-for="pageNumber in pageNumbers"
+            :key="pageNumber"
+            @click="changePage(pageNumber)"
+          >
+            <span class="page-link">{{ pageNumber }}</span>
+          </li>
+          <!-- <li class="page-item">
+                        <a class="page-link" href="#" aria-label="Next">
+                            <span aria-hidden="true">&raquo;</span>
+                        </a>
+                    </li> -->
+        </ul>
+      </nav>
+    </div>
+    <div class="alert alert-danger" role="alert" v-if="errored">error loading data</div>
+  </div>
 </template>
 <script>
-import axios from '@/sevice/axios';
+import axios from "@/sevice/axios";
 export default {
-    name: 'Index',
-    data(){
-        return {
-            name: '',
-            desks: [],
-            errored: false,
-            isLoading: false,
-            errorMessage: null
-        }
+  name: "Index",
+  data() {
+    return {
+      name: "",
+      desks: [],
+      errored: false,
+      isLoading: false,
+      errorMessage: null,
+      page: 1,
+      pageNumbers: 0,
+    };
+  },
+  mounted() {
+    this.getDesks();
+  },
+  methods: {
+    async getDesks() {
+      this.isLoading = true;
+      await axios
+        .get("/desks", {
+          params: {
+            page: this.page,
+          },
+        })
+        .then((response) => {
+          this.desks = response.data.data;
+          this.pageNumbers = response.data.info;
+          this.isLoading = false;
+        })
+        .catch((error) => {
+          this.errored = true;
+          this.isLoading = false;
+        });
     },
-    mounted(){
+    deleteDesk(id) {
+      if (confirm("Are you sure you want to delete")) {
+        axios
+          .post(`/desks/${id}`, {
+            _method: "DELETE",
+          })
+          .then((response) => {
+            alert(response.data.message);
+          })
+          .catch((error) => {
+            console.log(error);
+            alert("desk don`t delete something went wrong!");
+          })
+          .finally(() => {
+            this.getDesks();
+          });
+      }
+    },
+    changePage(page){
+        this.page = page
         this.getDesks()
     },
-    methods: {
-        async getDesks(){
-            this.isLoading = true
-            await axios.get('/desks').then(response => {
-                this.desks = response.data.data
-                this.isLoading = false
-            }).catch(error => {
-                this.errored = true
-                this.isLoading = false
-            })
-        },
-        deleteDesk(id){
-            if(confirm('Are you sure you want to delete')){
-                axios.post(`/desks/${id}`,{
-                    _method: 'DELETE'
-                }).then(
-                response => {
-                    alert(response.data.message)
-                },
-                ).catch(error => {
-                    console.log(error);
-                    alert('desk don`t delete something went wrong!')
-                }).finally(()=>{ 
-                    this.getDesks()
-                })
-            }
-        },
-        addDesk(){
-            axios.post('/desks',{name: this.name})
-            .then(response => {
-                alert(response.data.message);
-                this.name = ''
-                this.getDesks()
-            })   
-            .catch (error => {
-                this.errorMessage = error.response.data.message
-            })
-            .finally((event)=>{
-                setTimeout(() => {
-                    this.errorMessage = false
-                }, 5000);
-            })
-        }
+    addDesk() {
+      this.isLoading = true;
+      axios
+        .post("/desks", { name: this.name })
+        .then((response) => {
+          alert(response.data.message);
+          this.name = "";
+          this.getDesks();
+        })
+        .catch((error) => {
+          this.isLoading = false;
+          this.errorMessage = error.response.data.message;
+        })
+        .finally((event) => {
+          setTimeout(() => {
+            this.errorMessage = false;
+          }, 5000);
+        });
     },
-}
+  },
+};
 </script>
-<style>
-
-</style>
+<style></style>
--- src/components/desks/Show.vue
+++ src/components/desks/Show.vue
@@ -2,9 +2,6 @@
     <div class="container">
         <div class="form-group">
             <input type="text" v-model="desk" class="form-control">
-            <!-- <div class="invalid-feedback">
-                please choose a name
-            </div> -->
             <button class="btn btn-primary mt-3" @click.prevent="saveName">update</button>
         </div>
         <form class="mt-3">
@@ -28,9 +25,28 @@
         </div>
         <div class="row">
             <div class="col-lg-4" v-for="desk_list in desk_lists">
-                <div class="card mt-3">
-                <a href="#" class="card-body"><h5 class="card-title text-center">{{ desk_list.name }}</h5></a>
-                <button type="button" class="btn btn-danger" @click="deleteDeskList(desk_list.id)">Delete</button>
+                <div class="card bg-light mt-3">
+                    <div class="card-body">
+                        <form @submit.prevent="updateDeskList(desk_list.id, desk_list.name)" v-if="desk_list_input_id == desk_list.id" class="d-flex justify-content-between align-items-center">
+                            <input type="text" class="form-control my-3" v-model="desk_list.name" required>
+                            <button type="button" @click="desk_list_input_id = null" class="close border-0" area-label="Close">
+                                <span area-hidden="true">&times;</span>
+                            </button>
+                        </form>
+                        <h5 v-else class="card-title d-flex justify-content-between align-items-center" style="cursor: pointer;" @click="desk_list_input_id = desk_list.id">{{ desk_list.name }}<i class="fas fa-pencil-alt" style="font-size: 15px;"></i></h5>
+                    </div>
+                    <button type="button" class="btn btn-danger" @click="deleteDeskList(desk_list.id)">Delete</button>
+                    <div>
+                        <div class="card m-3 bg-success" v-for="card in desk_list.cards">
+                            <div class="card-body p-2">
+                                <h5 class="card-title">{{ card.name }}</h5>
+                            </div>
+                            <button type="button" class="btn btn-danger w-50 m-auto ">Delete</button>
+                        </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control" v-model="cardNames[desk_list.id]">
+                            </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -50,7 +66,9 @@ export default {
             errored: false,
             isLoading: false,
             desk_lists: '',
-            errorMessage: false
+            errorMessage: false,
+            desk_list_input_id: null,
+            cardNames: []
         }
     },
     mounted(){
@@ -58,6 +76,16 @@ export default {
         this.getDeskLists()
     },
     methods: {
+        updateDeskList(id, name){
+            this.isLoading = true
+            axios.post('/desk-lists/'+id, {
+                _method: 'PUT',
+                name
+            }).then(response => {
+                this.isLoading = false
+                this.desk_list_input_id = null
+            })
+        },
         getDeskLists(){
             axios.get('/desk-lists', {
                 params: {
@@ -65,7 +93,9 @@ export default {
                 }
             }).then(response => {
                 this.desk_lists = response.data.data
-                // console.log(response.data.data);
+                this.desk_lists.array.forEach(element => {
+                    this.cardNames[element.id] = ''
+                });
             })
         },
         async getDesk(){
@@ -87,9 +117,7 @@ export default {
                 alert(response.data.message);
                 this.isLoading = false
                 this.$router.push({name: 'desks'})
-            })// .catch(error => {
-            //     console.log(error);
-            // })
+            })
         },
         addDeskList(){
             this.isLoading = true
